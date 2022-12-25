606d2ba35cad95abe8d38e2f5078dc110a28165f
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-12-25 18:08:17 +0600

    Values and Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,23 @@
 let js = "amazing";
-if (js === "amazing") alert("Javascript is FUN!");
+
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "Jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let mySecondJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob);
