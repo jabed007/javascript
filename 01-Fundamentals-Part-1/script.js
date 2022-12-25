60498d29b54d93d54204e8330767223f4e2ed926
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-12-25 20:00:13 +0600

    Data types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,4 @@
+// Values and Variables
 let js = "amazing";
 
 console.log(40 + 8 + 23 - 10);
@@ -21,3 +22,37 @@ let job1 = "Programmer";
 let job2 = "Teacher";
 
 console.log(myFirstJob);
+
+// Assignment of Values and Variables
+let country = "Bangladesh";
+let continent = "Asia";
+let population = 166.3;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// Data types
+
+true;
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
