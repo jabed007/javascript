6ac412f9725f4eb91d1675c4ab62ca823d9b6b57
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-12-27 22:30:07 +0600

    Type conversion and Coercion

--- Jonas Schmedtmann Course Code/01-Fundamentals-Part-1/starter/script.js
+++ Jonas Schmedtmann Course Code/01-Fundamentals-Part-1/starter/script.js
@@ -228,3 +228,27 @@ if (BMIMark > BMIJohn) {
 } else {
   console.log(`John's BMI (${BMIJohn}) is higher than Marks's (${BMIMark})!`);
 }
+
+////////////////////////////////////
+// Type Conversion and Coercion
+
+console.log("-----Type Conversion and Coercion-----");
+
+// type conversion
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Jonas"));
+console.log(typeof NaN);
+
+// type coercion
+console.log("I am " + 23 + " years old");
+console.log("I am " + "23 " + " years old");
+
+console.log("23" - "10" - 3);
+console.log("23" / "2");
+
+let n = "1" + 1; // '11'
+n = n - 1;
+console.log(n);
