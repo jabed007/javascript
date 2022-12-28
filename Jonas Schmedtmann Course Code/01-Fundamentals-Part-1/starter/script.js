f54c3121fe80806481cf0430d77aea834473b726
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-12-28 20:23:31 +0600

    The Conditional (Ternary) Operator

--- Jonas Schmedtmann Course Code/01-Fundamentals-Part-1/starter/script.js
+++ Jonas Schmedtmann Course Code/01-Fundamentals-Part-1/starter/script.js
@@ -385,3 +385,18 @@ if (23 > 10) {
 }
 const me = "Jonas";
 console.log(`I'm ${2037 - 1991} years old ${me}`);
+
+////////////////////////////////////
+// The Conditional (Ternary) Operator
+const age2 = 23;
+// age2 >= 18 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');
+const drink = age2 >= 18 ? "wine 🍷" : "water 💧";
+console.log(drink);
+let drink2;
+if (age >= 18) {
+  drink2 = "wine 🍷";
+} else {
+  drink2 = "water 💧";
+}
+console.log(drink2);
+console.log(`I like to drink ${age >= 18 ? "wine 🍷" : "water 💧"}`);
