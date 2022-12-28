f1005606e61cab12c2e2d91a81cd1dc30b9ada1d
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-12-28 20:18:52 +0600

    Statements and expression

--- Jonas Schmedtmann Course Code/01-Fundamentals-Part-1/starter/script.js
+++ Jonas Schmedtmann Course Code/01-Fundamentals-Part-1/starter/script.js
@@ -374,3 +374,14 @@ if (day === "monday") {
 } else {
   console.log("Not a valid day!");
 }
+
+////////////////////////////////////
+// Statements and Expressions
+3 + 4;
+1991;
+true && false && !false;
+if (23 > 10) {
+  const str = "23 is bigger";
+}
+const me = "Jonas";
+console.log(`I'm ${2037 - 1991} years old ${me}`);
