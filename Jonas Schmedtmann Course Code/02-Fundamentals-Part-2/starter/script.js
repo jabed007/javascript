8a0ef19ac3115bb8ab39a00d84309d56c3178b76
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-12-30 21:11:17 +0600

    Javascript Function

---
+++ Jonas Schmedtmann Course Code/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+///////////////////////////////////////
+// Activating Strict Mode
+let hasDriverLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriverLicense) console.log("I can drive :D");
+
+// const interface = audio;
+// const private = 534;
+
+///////////////////////////////////////
+// Functions
+function logger() {
+  console.log("My name is Jonas");
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
+console.log(num);
