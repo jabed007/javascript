c8ac03857492392b520f9e93a843bccf193b0916
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2023-01-02 11:37:39 +0600

    Dot vs. Bracket Notation

--- Jonas Schmedtmann Course Code/02-Fundamentals-Part-2/starter/script.js
+++ Jonas Schmedtmann Course Code/02-Fundamentals-Part-2/starter/script.js
@@ -280,3 +280,38 @@ const jonas = {
   job: "Tecaher",
   friends: ["Michael", "Peter", "Steven"],
 };
+
+///////////////////////////////////////
+// Dot vs. Bracket Notation
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas["lastName"]);
+
+const nameKey = "Name";
+console.log(jonas["first" + nameKey]);
+console.log(jonas["last" + nameKey]);
+
+//console.log(jonas.'last' + nameKey);
+const interestedIn = prompt(
+  "What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends"
+);
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  console.log(
+    "Wrong request! Choose between firstName, lastName, age, job and friends"
+  );
+}
+
+jonas.location = "Portugal";
+console.log(jonas["location"]);
+jonas["twitter"] = "@jonasschmedtmann";
+console.log(jonas);
+
+// Challenge
+// "Jonas has 3 friends, and his best friend is called Michael"
+console.log(
+  `${jonas["firstName"]} has ${jonas["friends"].length} friends, and his best freind is called ${jonas.friends[0]}`
+);
