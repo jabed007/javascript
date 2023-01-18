e76f01e2ca0dfc6d12c0f9a1282a63f20efe98b4
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2023-01-18 12:15:01 +0600

    example of factory function

---
+++ Mosh Hamedani Course Code/04_Objects/script.js
@@ -0,0 +1,74 @@
+// Objects
+// Objects are collections of key value pairs
+
+// So, if we have properties that are highly related, we want to encapsulate them inside of an object
+
+// As an  example, lets say we are building an application for drawing different kinds of shapes, like circles, rectangles and so on.
+
+// So, we could declare multiple variables, around circles for example radius we set that to 1. let  x = 1, y = 1, we are defining multiple variables, but all these variables are highly related, they represents the circle. A better approach is to put these variables inside of an object.
+
+// Now we can send that object anywhere in our programs, we can pass that to any functions, and all these variables would be avilable in that object.
+
+// The value we have in an object can be any type in Javascript, It can be a number, a string,  a boolean, null, undefined, it can even be another object, or an array, or a function.
+
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  isVisible: true,
+  draw: function () {
+    console.log("Draw");
+  },
+};
+
+const circle2 = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  isVisible: true,
+  draw: function () {
+    console.log("Draw");
+  },
+};
+
+// Factory or Constructor functions
+
+// Factory Functions is just like a factory producing products.
+// These factory functions produce objects
+
+// function createCircle() {
+//   const circle = {
+//     radius: 1,
+//     location: {
+//       x: 1,
+//       y: 1,
+//     },
+//     isVisible: true,
+//     draw: function () {
+//       console.log("draw");
+//     },
+//   };
+//   return circle;
+// }
+
+function createCircle(radius) {
+  return {
+    radius,
+    location: {
+      x: 1,
+      y: 1,
+    },
+    isVisible: true,
+    draw() {
+      console.log("draw");
+    },
+  };
+}
+const circle1 = createCircle(10);
+//console.log(circle1);
+circle1.draw();
+//circle.draw(); // Method
