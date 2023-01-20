65886ae1870a5d42e44b625e0199c5c05e4668e4
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2023-01-20 16:44:28 +0600

    Finding Elements (Primitives)

--- Mosh Hamedani Course Code/05_Arrays/script.js
+++ Mosh Hamedani Course Code/05_Arrays/script.js
@@ -6,7 +6,7 @@
 // Combinning Arrays
 
 // Adding Elements
-const numbers = [3, 4];
+const numbers = [1, 2, 3, 4];
 // numbers = []; // error
 // Here we declared an array as constant, that means we can not reassign numbers to something else, if we do this, we get this error assignment to constant variable.
 // However, its perfectly fine to modify the content this array.
@@ -27,3 +27,14 @@ console.log(numbers);
 // Middle
 numbers.splice(2, 0, "a", "b");
 console.log(numbers);
+
+// Finding Elements (Primitives)
+// Finding elements depends on if you're storing primitive or reference types in an array
+console.log(numbers.indexOf("x"));
+console.log(numbers.indexOf(1));
+console.log(numbers.indexOf("1"));
+console.log(numbers.lastIndexOf(1));
+
+// If a given element exist we can check this by
+console.log(numbers.indexOf(1) !== -1); // this is a little bit ugly, we have a new method in javascript for achieving the same thing
+console.log(numbers.includes(1));
