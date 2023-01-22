eac7be8471559ea119e38bbd14f5512607307784
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2023-01-22 10:55:51 +0600

    Finding elements reference types(array.find() and array.findIndex() method)

--- Mosh Hamedani Course Code/05_Arrays/script.js
+++ Mosh Hamedani Course Code/05_Arrays/script.js
@@ -38,3 +38,27 @@ console.log(numbers.lastIndexOf(1));
 // If a given element exist we can check this by
 console.log(numbers.indexOf(1) !== -1); // this is a little bit ugly, we have a new method in javascript for achieving the same thing
 console.log(numbers.includes(1));
+
+// Finding Elements (Reference Types)
+// Finding primitives is different from finding reference types
+
+const courses = [
+  { id: 1, name: "a" },
+  { id: 2, name: "b" },
+];
+
+console.log(courses.includes({ id: 1, name: "a" })); // output: false
+// we dont use .includes() method to find a value of an object
+// instead we use object.find() method to finding elements of an object
+
+const course = courses.find(function (course) {
+  return course.name === "a";
+});
+
+console.log(course);
+
+const courseIndex = courses.findIndex(function (course) {
+  return course.name === "b";
+});
+
+console.log(courseIndex);
