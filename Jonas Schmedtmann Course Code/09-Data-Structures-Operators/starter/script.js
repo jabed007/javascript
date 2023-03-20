7d1065d3919f7b265a9e7820df65bf47c5f2f7a0
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2023-03-20 22:45:48 +0600

    Object Destructuring

--- Jonas Schmedtmann Course Code/09-Data-Structures-Operators/starter/script.js
+++ Jonas Schmedtmann Course Code/09-Data-Structures-Operators/starter/script.js
@@ -29,8 +29,66 @@ const restaurant = {
       close: 24,
     },
   },
+
+  orderDelivery: function ({
+    address,
+    time = '20:20',
+    starterIndex = 1,
+    mainIndex = 0,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
 
+restaurant.orderDelivery({
+  mainIndex: 2,
+  starterIndex: 2,
+  time: '20:00',
+  address: 'xyz',
+});
+
+restaurant.orderDelivery({ address: 'abc', starterIndex: 1 });
+
+// Destructuring Object
+// Destructuring object is very helpful when we deal with the result of an API call, which basically means to get data from another web application, like waether data or data about movies or something like that and this data usually comes in the form of objects basically. and destructuring is really a lifesaver.
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+// What if we wanted the variable names to be different from the property names?
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+
+console.log(restaurantName, hours, tags);
+
+// Default Values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating Variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested Objects
+/*
+const { fri } = openingHours;
+console.log(fri);
+*/
+
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+/*
 // Destructuring Array
 
 const arr = [1, 2, 3];
@@ -74,3 +132,4 @@ console.log(i, j, k);
 // Default Values
 const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r);
+*/
